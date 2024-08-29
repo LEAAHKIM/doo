e44f35bc77c0c42779f9bec12440126a805d06d0
LEAAHKIM <lk2935@barnard.edu>
2024-08-29 23:05:18 +0900

    Initial Commit

---
+++ doo/dooApp.swift
@@ -0,0 +1,17 @@
+//
+//  dooApp.swift
+//  doo
+//
+//  Created by Leah Kim on 8/29/24.
+//
+
+import SwiftUI
+
+@main
+struct dooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
